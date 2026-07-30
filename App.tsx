87c5f98b047993aa3c42f9a9d9155627a1309b49
Paulo Ruszel <paulo.ruszel.santos@gmail.com>
2026-07-29 21:03:38 -0300

    refactor: remover polyfill global de alertas web

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import { Alert, Platform } from 'react-native';
+import { Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
 import FlashMessage from "react-native-flash-message";
 import { colors } from "./src/theme";
-import { installWebAlertPolyfill } from "./src/utils/webAlert";
-
-installWebAlertPolyfill(Alert, Platform.OS === "web");
 
 const linking = {
   prefixes: ["obradocs://"],
